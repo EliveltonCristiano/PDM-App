1c3dc6b37da7406006a5d309cf8c0535ba081a25
Elivelton Cristiano <eliveltoncristiano@gmail.com>
2020-12-11 17:27:46 -0300

    Add files via upload

---
+++ pages/Cadastro.vue
@@ -0,0 +1,61 @@
+/* A tela de CADASTRO deverá conter ao menos:
+
+- Nome
+- Email
+- Senha
+- Celular
+- Botão de cadastro */
+
+
+<template>
+    <view class="container">
+         <text class="titulo">Cadastrar usuario</text>
+
+         <text-input class="input" v-model="Nome" />
+         <text-input class="input" v-model="Email" />
+         <text-input class="input" v-model="Senha" />
+         <text-input class="input" v-model="Celular" />
+         
+        <button
+            class="button"
+            :on-press="onPressCadastrar"
+            title="Cadastrar"
+        />
+
+    </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    onPressCadastrar: function() {
+      this.navigation.navigate("MeusDados");
+        }  
+    }
+}
+</script>
+
+<style>
+.container {
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.titulo {
+  justify-content: flex-start;
+  font-size: 30px;
+  text-align: center;
+}
+.input {
+    background-color: white;
+    height: 40; 
+    width: 300;
+    margin-top: 5px; 
+    borderWidth: 0.9;
+}
+</style>
---
+++ pages/Login.vue
@@ -0,0 +1,76 @@
+
+/*  A tela de LOGIN deverá conter:
+
+- Email
+- Senha
+- Um link para "Esqueci Minha Senha"
+- Botão de Login */
+
+<template>
+  <view class="container">
+    
+    <text class="titulo">Login</text>
+    <text-input class="input" v-model="nome" />
+    <text-input class="input" v-model="senha" />
+
+    <touchable-opacity :on-press="onPressRecuperarSenha">
+      <text> 
+        Esqueceu a senha? 
+      </text> 
+    </touchable-opacity>
+
+    <button 
+      class="button" 
+      v-bind:title="message" 
+      v-bind:on-press="onPressLogin"
+    />
+   
+  </view>
+</template>
+
+<script>
+export default { 
+  data: function()  {
+    return{
+      message: "Login",
+      nome: "Usuario ou Email",
+      senha: "Senha"
+    };
+  },
+    props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    onPressLogin: function() {
+      //alert('Acesso negado.');
+      this.navigation.navigate("Cadastro");
+    },
+    onPressRecuperarSenha: function() {
+      alert('Informe o email cadastrado.')
+    } 
+  },
+};
+</script>
+
+<style>
+.container {
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.titulo {
+  justify-content: flex-start;
+  font-size: 30px;
+  text-align: center;
+}
+.input {
+  background-color: white;
+  height: 40; 
+  width: 300;
+  margin-top: 5px; 
+  borderWidth: 0.9;
+}
+
+</style>
