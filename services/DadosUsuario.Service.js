741e24d349373ed197824563be57489ee61b20e1
Elivelton Cristiano <eliveltoncristiano@gmail.com>
2020-12-17 22:16:37 -0300

    Update DadosUsuario.Service.js

--- services/DadosUsuario.Service.js
+++ services/DadosUsuario.Service.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-axios.get('https://us-central1-uncisal.cloudfunctions.net/users-get?id=545')
-.then(function (response) {
-    console.log("_____");
-    usuario.nome = response.data.usuario.nome;
-})
-.catch(function (error) {
-  console.log(error);
-});
+export default class DadosUsuarioService {
+  static async getDados() {
+    const { data } = await axios.get("https://us-central1-uncisal.cloudfunctions.net/users-get?id=545")
+    .then(function (response) {
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
